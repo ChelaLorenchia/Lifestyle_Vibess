c895867ad110edf6ca6011e69a8bbf6c269ef86a
ChelaLorenchia <160341905+ChelaLorenchia@users.noreply.github.com>
2024-10-28 04:26:00 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// JavaScript for handling subscribe button
+document.getElementById("subscribeButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("Thank you for subscribing!");
+  });
+  
